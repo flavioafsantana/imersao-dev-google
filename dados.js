3594cc1cffa8539a9d134537703b90d46b3ea1ff
flavioafsantana <flavioafsantana@gmail.com>
2024-09-05 00:07:38 -0300

    atualizando codigos aula 3

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Mercedes",
+        descricao: "A Mercedes F1, com uma história rica e vitoriosa na Fórmula 1, dominou a era híbrida da categoria, conquistando diversos títulos consecutivos de pilotos e construtores. Com carros extremamente competitivos e pilotos como Lewis Hamilton e Nico Rosberg, a equipe alemã se tornou um ícone da Fórmula 1. Atualmente, a Mercedes busca manter sua competitividade e conquistar novos títulos para consolidar seu legado.",
+        link: "https://www.mercedesamgf1.com/"
+    },
+    {
+        titulo: "Ferrari",
+        descricao: "A Ferrari, a equipe mais antiga e icônica da Fórmula 1, possui uma história rica em títulos e paixão. Com uma legião de fãs em todo o mundo, a Scuderia Ferrari busca constantemente a excelência e a conquista de novos títulos mundiais. Pilotos lendários como Michael Schumacher e Fernando Alonso já defenderam as cores da Ferrari, e a equipe italiana continua a ser uma das mais populares e admiradas da categoria.",
+        link: "https://www.ferrari.com/en-US/formula1"
+    },
+    {
+        titulo: "Red Bull Racing",
+        descricao: "A Red Bull Racing, nascida das cinzas da equipe Jaguar em 2005, rapidamente se tornou uma força dominante na Fórmula 1. Com uma combinação de engenharia de ponta, pilotos talentosos e uma estratégia agressiva, a equipe austríaca conquistou diversos títulos mundiais de pilotos e construtores nas décadas de 2010 e 2020. Nomes como Sebastian Vettel e Max Verstappen marcaram época à frente dos carros da Red Bull, levando a equipe a um período de grande sucesso e transformando-a em uma das mais populares e admiradas da categoria. Atualmente, a Red Bull continua a ser uma das principais forças da Fórmula 1, com Verstappen dominando o grid e a equipe buscando novos títulos para consolidar seu legado.",
+        link: "https://www.redbullracing.com/int-en"
+    }
+];
